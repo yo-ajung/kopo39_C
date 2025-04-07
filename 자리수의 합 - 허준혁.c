b773bde33713b01398204fb6e07d3a3866c34f3f
hyeok <gjwnsgur0923@naver.com>
2025-04-07 14:26:36 +0900

    Update 자리수의 합 - 허준혁.c

--- 자리수의 합 - 허준혁.c
+++ 자리수의 합 - 허준혁.c
@@ -9,7 +9,7 @@ void main()
     int b = (num % 100) / 10;
     int c = (num % 10) / 1;
 
-    if(num >= 1000) {
+    if (num >= 1000) {
         printf("1000이상은 입력할 수 없습니다.");
     } else {
         printf("자리 수의 합: %d", a + b + c);    
