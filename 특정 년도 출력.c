d62dfc08bee2464a0749bc6858aaffc0c66ea538
hyeok <gjwnsgur0923@naver.com>
2025-04-14 10:36:43 +0900

    Update and rename 특정 년도 출력.txt to 특정 년도 출력.c

--- 특정 년도 출력.c
+++ 특정 년도 출력.c
@@ -6,17 +6,14 @@ void main()
     int month[12] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}; //월
     
     for (int i = 0; i < 6; i++) {
-        for(int j = 0; j < 12; j++) {
-            if(year[i] == 2023 && (month[j] == 2 || month[j] == 3 || month[j] == 4 || month[j] == 5)) {
+        for (int j = 0; j < 12; j++) {
+            if (year[i] == 2023 && (month[j] == 2 || month[j] == 3 || month[j] == 4 || month[j] == 5)) {
                 continue;
             }
             printf("%d년 %d월\n", year[i], month[j]);
-            
-            if(year[i] == 2025 && month[j] == 4) {
+            if (year[i] == 2025 && month[j] == 4) {
                 break;
             }
         }
     }
 }
-
-특정 년도 월 출력하기
