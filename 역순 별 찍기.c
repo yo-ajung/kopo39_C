1c8671cebc0b37fce299ca1dcf51d940610f4d50
hyeok <gjwnsgur0923@naver.com>
2025-04-14 15:38:15 +0900

    Update 역순 별 찍기.c

--- 역순 별 찍기.c
+++ 역순 별 찍기.c
@@ -2,7 +2,10 @@
 
 void main()
 {
-    for (int i = 5; i > 0; i--) {
+    int num = 0;
+    scanf("%d", &num);
+    
+    for (int i = num; i > 0; i--) {
         for (int j = 0; j < i; j++) {
             printf("*");
         }
